a3b29b4ac7e7f5ce27fda4ee435b265b7828eeb1
Ken Yang <kenyangzq95@outlook.com>
2026-01-31 10:57:16 -0800

    Initial Commit

---
+++ NihonGoStart/NihonGoStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  NihonGoStartApp.swift
+//  NihonGoStart
+//
+//  Created by Ken Yang on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct NihonGoStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NihonGoStartTests/NihonGoStartTests.swift
@@ -0,0 +1,17 @@
+//
+//  NihonGoStartTests.swift
+//  NihonGoStartTests
+//
+//  Created by Ken Yang on 1/31/26.
+//
+
+import Testing
+@testable import NihonGoStart
+
+struct NihonGoStartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
